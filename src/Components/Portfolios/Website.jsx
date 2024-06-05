28fea7f9de16171d177daadcc97362524ced762b
ekosetia1 <setyawane152@gmail.com>
2024-06-06 02:26:31 +0700

    merubah sumber gambar pada file portfolio

--- src/Components/Portfolios/Website.jsx
+++ src/Components/Portfolios/Website.jsx
@@ -3,8 +3,8 @@ import PortfolioSection from "../Page/Portfolio";
 function Website() {
   return (
     <PortfolioSection>
-      <CardPortofolio img="web-1" />
-      <CardPortofolio img="web-2" />
+      <CardPortofolio img="web-1.jpg" />
+      <CardPortofolio img="web-2.jpg" />
     </PortfolioSection>
   );
 }
