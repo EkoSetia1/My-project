fd7ae61a44d95c547a2c4593160efd33a880e8ee
ekosetia1 <setyawane152@gmail.com>
2024-06-06 02:40:40 +0700

    merubah sumber file gambar dengan import file modular

--- src/Components/Portfolios/Website.jsx
+++ src/Components/Portfolios/Website.jsx
@@ -1,10 +1,13 @@
 import CardPortofolio from "../Atom/CardPortofolio";
 import PortfolioSection from "../Page/Portfolio";
+
+import Web1 from "../../assets/web-1.jpg";
+import Web2 from "../../assets/web-2.jpg";
 function Website() {
   return (
     <PortfolioSection>
-      <CardPortofolio img="web-1.jpg" />
-      <CardPortofolio img="web-2.jpg" />
+      <CardPortofolio img={Web1} />
+      <CardPortofolio img={Web2} />
     </PortfolioSection>
   );
 }
